c70072e2dadce5a4abc2ab819cd43ee2ea24ea56
Nick <nick_zaidan@hotmail.com>
2026-05-28 18:25:54 +1000

    Added better css and more questions

--- src/questions.js
+++ src/questions.js
@@ -20,11 +20,6 @@ export const QUESTIONS = [
     options: ["Yes", "No"],
   },
   {
-    id: "bouquet",
-    text: "Who will catch the bouquet?",
-    options: ["Friend", "Family", "Nobody", "Surprise result"],
-  },
-  {
     id: "dancefloor",
     text: "Who will be first on the dance floor?",
     options: ["Bride", "Groom", "Parents", "Bridal party", "Guest"],
@@ -38,5 +33,30 @@ export const QUESTIONS = [
     id: "song",
     text: "Will Mr Brightside play?",
     options: ["Yes", "No"],
+  },
+  {
+    id: "tie",
+    text: "Which groomsmen will be the first to take off their tie?",
+    options: ["Christopher Zaidan", "Matthew Zaidan", "Henry Keegan", "Ethan Kelly", "Ethan Shaw"],
+  },
+  {
+    id: "cake-flavour",
+    text: "What flavour will the bottom tier of the wedding cake be?",
+    options: ["Chocolate and Hazelnut", "Caramel and Chocolate Swirl", "Strawberry and Vanilla", "Red Velvet"],
+  },
+  {
+    id: "bridesmaid-cry",
+    text: "Which bridemaid will cry the most throughout the night?",
+    options: ["Patsy/Poopsy Morales", "Meera Kunathasan", "Kanchi Devandran", "Cimaine Hoang", "Carolyn Qiu"],
+  },
+  {
+    id: "taxi",
+    text: "How many times will someone correctly yell 'Taxi!' throughout the night?",
+    options: ["0-3", "4-6", "6-10", "10+"],
+  },
+  {
+    id: "song-choice",
+    text: "What was the most requested song for the dancefloor?",
+    options: ["Mr Brightside by The Killers", "Mama Mia by Abba", "Levels by Avicii", "The Habibi Song by Pauline Robinson", "In The End by Linkin Park"],
   },
 ];
